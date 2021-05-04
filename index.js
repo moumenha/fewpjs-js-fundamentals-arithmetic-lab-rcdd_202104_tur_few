005ce0f18e86374f95765a26b6bf919997288045
Moumen Hallak <moumen95@gmail.com>
2021-05-04 01:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 
 let num1 = 2;
 let num2 = 31;
-
+let num3 = 8;
+let num4 = 2;
 let multiply = num1 * num2;
 
 
 let random = Math.floor(Math.random() * 10) + 1;
 
-let mod = 
+let mod = num3 % num4;
