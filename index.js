f15e5406b9cebece13ac3b48dc64522bb54a8857
Moumen Hallak <moumen95@gmail.com>
2021-05-04 01:38:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let num3 = 59;
+let num4 = 11;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,5,20);
